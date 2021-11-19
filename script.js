846846ad87738c1b65e58bdfbebae5a3ed1257b5
Alex <lubu221996@gmail.com>
2021-11-19 17:42:25 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* const category = 'toys';
+
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'ivan';
+
+alert(`привет ${user}`); */
+
+console.log('arr' + ' - object');
+
+let incr = 10,
+    decr = 10;
+/* incr++;
+decr--; */
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === '8');
+
+
+const isCheked = true,
+        isClose = false;
+console.log(isCheked || !isClose);
