79fd05b72a3a144700a0bdb6f531371ed663a3e8
Alex <lubu221996@gmail.com>
2021-11-20 19:02:40 +0500

    for

--- script.js
+++ script.js
@@ -1,54 +1,19 @@
 "use strict";
 
-/* const numberOfFilms = prompt('Сколько фильмов вы уже посмтотрели?', '');
+let num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genre: {},
-    privat: false
-};
+/* while (num < 55) {
+    console.log(num);
+    num++;
+} */
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-    b = prompt('На сколько оцените его?', ''),
-    c = prompt('Один из просмотренных фильмов?', ''),
-    d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB); */
-
-/* if (3 - 5 == 9) {
-    console.log('Ok');
-} else {
-    console.log('You eblan');
+/* do {
+    console.log(num);
+    num++;
 }
- */
-const num = 50;
+while (num < 55); */
 
-/* if (num < 49) {
-    console.log('Error');
-} else if (num > 100) {
-    console.log('Много');
-} else {
-    console.log("Ok!");
-}
-
-(num === 50) ? console.log('Ok') : console.log('Error'); */
-
-
-switch (num) {
-    case 49: 
-        console.log('No');
-        break;
-    case 100: 
-        console.log('No no');
-        break;
-    case 50:
-        console.log('Yes');
-        break;
-    default:
-        console.log('NO nono');
+for (let i = 1; i < 8; i++) {
+    console.log(num);
+    num++;
 }
