6fe7df1a9f1b5f068053913380b13ab5c80fffd6
Alex <lubu221996@gmail.com>
2021-11-20 22:57:46 +0500

    movie_2.0

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 "use strict";
 
-let num = 50;
+"use strict";
 
-/* while (num < 55) {
-    console.log(num);
-    num++;
-} */
+const numberOfFilms = prompt('Сколько фильмов вы уже посмтотрели?', '');
 
-/* do {
-    console.log(num);
-    num++;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genre: {},
+    privat: false
+};
+
+for (let i = 0; i < 2; i++) {
+const a = prompt('Один из просмотренных фильмов?', ''),
+        b = prompt('На сколько оцените его?', '');
+
+        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log("error");
+            i--;
+        }
 }
-while (num < 55); */
 
-for (let i = 1; i < 10; i++) {
-    if (i === 8) {
-        /* break */
-        continue;
-    }
-    console.log(i);
-    num++;
+if (personalMovieDB.count < 10) {
+    console.log("Просмотрено мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    console.log("Вы посмотрели много фильмов");
+} else {
+    console.log('Error');
 }
+console.log(personalMovieDB);
