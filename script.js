143f4953cf25516a23893da145a917ab7a657b4a
Alex <lubu221996@gmail.com>
2021-11-21 17:49:38 +0500

    callback_functions

--- script.js
+++ script.js
@@ -1,68 +1,26 @@
 "use strict";
 
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмтотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмтотрели?', '');
-
-    }
+function first() {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 5000);
 }
 
-start();
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genre: {},
-    privat: false
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-const a = prompt('Один из просмотренных фильмов?', ''),
-        b = prompt('На сколько оцените его?', '');
-
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 ) {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log("error");
-            i--;
-        }
-    }
+function second() {
+    console.log(2);
 }
 
-rememberMyFilms();
+first();
+second();
 
-function detectPersonalLevel () {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы посмотрели много фильмов");
-    } else {
-        console.log('Error');
-    }
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
-detectPersonalLevel ();
 
-function showMyDB (hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
+function done() {
+    console.log('Я прошел этот урок!');
 }
-showMyDB ();
-function writeYourGanres() {
-    for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
-        personalMovieDB.genre[i - 1] = genre;
-    }
-}
-writeYourGanres();
 
+learnJS('JavaScript', done);
