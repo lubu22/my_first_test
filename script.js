c11aa7b0be31b02f75988f61920a60683789fb3c
Alex <lubu221996@gmail.com>
2021-11-23 17:03:44 +0500

    spread-operators

--- script.js
+++ script.js
@@ -1,32 +1,95 @@
 "use strict";
 
-const arr = [1, 28, 3, 6, 8];
-arr.sort(compareNum);
-console.log(arr);
+let a = 5,
+    b = a;
 
-function compareNum(a, b) {
-    return a - b;
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+/* const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+console.log(obj); */
+
+function copy(mainObj) {
+    let objCopy = {};
+
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+
+    return objCopy;
+} 
+
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+console.log(numbers);
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+/* console.log(add);
+console.log(clone); */
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'fsdfsdf';
+
+console.log(newArray);
+console.log(oldArray);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'liveejournal', 'blogger'],
+    internet = [...video, ...blogs, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
 }
 
-/* arr[99] = 0; */
-/* arr.push(10);
+const num = [2, 5, 7];
 
-console.log(arr); */
-/* 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
-}
- */
+log(...num);
 
-/* arr.forEach(function (item, i, arr) {
-    console.log(`${i}: ${item} внутри массива ${arr}`);
-});
+const array = ['a', 'b'];
+const newAarray = [...array];
 
-for (let value of arr) {
-    console.log(value);
-}
+const q = {
+    one: 1,
+    two: 2
+};
 
-const str = prompt('', '');
-const products = str.split(", ");
-products.sort();
-console.log(products.join('; ')); */
+const newObj = {...q};
